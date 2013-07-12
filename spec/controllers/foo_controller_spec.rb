7aa75187e6dfe45ac8f5b1cd750ea200251c7461
David Chen <mvjome@gmail.com>
2013-07-12 10:33:55 +0800

    puts controller definitions in spec_helper.rb; Add redirect_to rspec

--- spec/controllers/foo_controller_spec.rb
+++ spec/controllers/foo_controller_spec.rb
@@ -2,87 +2,6 @@
 
 require 'spec_helper'
 
-# https://www.relishapp.com/rspec/rspec-rails/docs/controller-specs/engine-routes-for-controllers
-
-module Dummy
-  class Application < Rails::Application
-  end
-end
-
-class BarController < ActionController::Base
-  # Fix In order to use #url_for, you must include routing helpers explicitly. For instance, `include Rails.application.routes.url_helpers
-  # thx https://github.com/apotonick/apotomo/issues/35#issuecomment-3936941
-  # TODO blankslate.rails_engine.rspec.gem
-  include Rails.application.routes.url_helpers
-  def _routes; ::Rails.application.routes end
-  def controller; parent_controller end
-
-end
-
-class FooController < BarController
-  include Stepstepstep
-
-  step :two => [:one_point_three, :one_point_seven] do
-    @a << 2
-  end
-  step :one_point_three => :one do
-    @a << 1.3
-  end
-  step :one do
-    @a = [1]
-  end
-  step :one_point_seven => :one_point_three, :only => :index do
-    @a << 1.7
-  end
-
-  def index
-    render :inline => @a.inspect
-  end
-
-  def another
-    @a << 'another'
-    render :inline => @a.inspect
-  end
-end
-
-class RemoveController < FooController
-  skip_filter :one_point_three
-end
-class AfterRemoveController < FooController
-end
-class InsertController < FooController
-  step :one_point_one => :one do
-    @a << 1.1
-  end
-  step :one_point_three => :one_point_one
-end
-class SubController < FooController
-  step :only_action => :two, :only => :another do
-    @a << 3
-  end
-
-  def another
-    @a << 'another'
-    render :inline => @a.inspect
-  end
-end
-class Sub2Controller < SubController
-  step :one_point_three, :except => [:index]
-  step :two => [:one, :one_point_seven] # remember to connect the broken edges
-end
-
-Dummy::Application.routes.draw do
-  [:bar, :remove, :after_remove, :insert, :sub2].each do |s|
-    resources s
-  end
-  [:foo, :sub].each do |r|
-    resources r do
-      collection do
-        get :another
-      end
-    end
-  end
-end
 
 describe FooController do
   it "@a is sort correctly" do
@@ -134,5 +53,13 @@ describe Sub2Controller do
   it "one_point_three should be excepted" do
     get :index
     response.body.should == [1, 1.7, 2].inspect
+  end
+end
+
+
+describe RedirectToController do
+  it "support redirect_to" do
+    get :index
+    response.should redirect_to("/redirect_to")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,94 @@ require 'stepstepstep'
 
 Rails.logger ||= Logger.new($stderr)
 ENV['DEBUG_STEPSTEPSTE'] = 'true' if `whoami`.strip == 'mvj3'
+
+
+# https://www.relishapp.com/rspec/rspec-rails/docs/controller-specs/engine-routes-for-controllers
+
+module Dummy
+  class Application < Rails::Application
+  end
+end
+
+class BarController < ActionController::Base
+  # Fix In order to use #url_for, you must include routing helpers explicitly. For instance, `include Rails.application.routes.url_helpers
+  # thx https://github.com/apotonick/apotomo/issues/35#issuecomment-3936941
+  # TODO blankslate.rails_engine.rspec.gem
+  include Rails.application.routes.url_helpers
+  def _routes; ::Rails.application.routes end
+  def controller; parent_controller end
+
+end
+
+class FooController < BarController
+  include Stepstepstep
+
+  step :two => [:one_point_three, :one_point_seven] do
+    @a << 2
+  end
+  step :one_point_three => :one do
+    @a << 1.3
+  end
+  step :one do
+    @a = [1]
+  end
+  step :one_point_seven => :one_point_three, :only => :index do
+    @a << 1.7
+  end
+
+  def index
+    render :inline => @a.inspect
+  end
+
+  def another
+    @a << 'another'
+    render :inline => @a.inspect
+  end
+end
+
+class RemoveController < FooController
+  skip_filter :one_point_three
+end
+class AfterRemoveController < FooController
+end
+class InsertController < FooController
+  step :one_point_one => :one do
+    @a << 1.1
+  end
+  step :one_point_three => :one_point_one
+end
+class SubController < FooController
+  step :only_action => :two, :only => :another do
+    @a << 3
+  end
+
+  def another
+    @a << 'another'
+    render :inline => @a.inspect
+  end
+end
+class Sub2Controller < SubController
+  step :one_point_three, :except => [:index]
+  step :two => [:one, :one_point_seven] # remember to connect the broken edges
+end
+
+class RedirectToController < FooController
+  def index
+    redirect_to "/redirect_to" and return
+  end
+end
+
+Dummy::Application.routes.draw do
+  [:bar, :remove, :after_remove, :insert, :sub2, :redirect_to].each do |s|
+    resources s
+  end
+  [:foo, :sub].each do |r|
+    resources r do
+      collection do
+        get :another
+      end
+    end
+  end
+end
+
+
